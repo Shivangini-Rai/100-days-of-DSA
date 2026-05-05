356207b62c7ab848a4d5b36bf6fbe44d112fe9a3
Shivangini-Rai <shivanginirai96@gmail.com>
2026-05-05 10:11:59 +0530

    Add files via upload

---
+++ day6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    // Read size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read array
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Edge case
+    if (n == 0) return 0;
+
+    int i = 0; // points to last unique element
+
+    for (int j = 1; j < n; j++) {
+        if (arr[j] != arr[i]) {
+            i++;
+            arr[i] = arr[j];
+        }
+    }
+
+    // Print unique elements
+    for (int k = 0; k <= i; k++) {
+        printf("%d ", arr[k]);
+    }
+
+    return 0;
+}
