12a3fb7baaf1fb9a3367237f17429a2d7bd38d5e
Shivangini-Rai <shivanginirai96@gmail.com>
2026-05-05 10:09:26 +0530

    Add files via upload

---
+++ day3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int n, k;
+
+    // Read size
+    scanf("%d", &n);
+
+    int arr[n];
+
+    // Read array elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Read key
+    scanf("%d", &k);
+
+    int comparisons = 0;
+    int foundIndex = -1;
+
+    // Linear Search
+    for (int i = 0; i < n; i++) {
+        comparisons++;
+        if (arr[i] == k) {
+            foundIndex = i;
+            break;
+        }
+    }
+
+    // Output
+    if (foundIndex != -1) {
+        printf("Found at index %d\n", foundIndex);
+    } else {
+        printf("Not Found\n");
+    }
+
+    printf("Comparisons = %d\n", comparisons);
+
+    return 0;
+}
